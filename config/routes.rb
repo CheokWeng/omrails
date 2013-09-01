06d59196d5cc5cc7bb6a5bff0ab27a74c40692c0
Peter Wong <cheokweng@hotmail.com>
2013-09-01 23:26:49 +0800

    Add Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Omrails::Application.routes.draw do
+  get 'about' => 'pages#about'
+  get 'faq' => 'pages#faq'
+
+  root 'pages#home'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
